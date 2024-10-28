39c82cc86ff1b1e9eac25d4367f8a2c3b5a503ce
Saurav Trivedi <ksaurav3045@gmail.com>
2024-10-29 05:08:39 +0530

    Added sync script

--- aqi-frontend/sync_aqi_build_folder_from_mac.sh
+++ aqi-frontend/sync_aqi_build_folder_from_mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define local and remote paths
-LOCAL_FOLDER_PATH="~/aqi-monitor/aqi-monitor/aqi-frontend/build"  # Replace with the local folder path
+LOCAL_FOLDER_PATH="/Users/saurav.kumar/aqi-monitor/aqi-monitor/aqi-frontend/build"  # Replace with the local folder path
 REMOTE_USER="ec2-user"
 REMOTE_HOST="98.130.74.124"
 REMOTE_PATH="/home/ec2-user/workdir/aqi-monitor/aqi-frontend/"
