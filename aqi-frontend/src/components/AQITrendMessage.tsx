5cbec5aefef0a191c0fcfde0282dc90886e69583
Saurav Trivedi <ksaurav3045@gmail.com>
2024-10-31 02:15:46 +0530

    bug fix in aqi calculation

--- aqi-frontend/src/components/AQITrendMessage.tsx
+++ aqi-frontend/src/components/AQITrendMessage.tsx
@@ -37,8 +37,8 @@ const AQITrendMessage: React.FC<AQITrendMessageProps> = ({ data }) => {
     const minAQI = Math.min(...recentData.map(point => (point.aqi_pm25 + point.aqi_pm10) / 2));
     const latestAQI = (recentData[recentData.length - 1].aqi_pm25 + recentData[recentData.length - 1].aqi_pm10) / 2;
 
-    const trendText = slope < -0.4 ? "Improving" : slope > 0.4 ? "Worsening" : "Stable";
-    const trendColor = slope < -0.4 ? "green" : slope > 0.4 ? "red" : "gray";
+    const trendText = slope < -1 ? "Improving" : slope > 1 ? "Worsening" : "Stable";
+    const trendColor = slope < -1 ? "green" : slope > 1 ? "red" : "gray";
 
     return (
         <Card style={{ textAlign: 'center', border: '1px solid #d9d9d9', borderRadius: '8px' }}>
