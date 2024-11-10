b83e122f3bc57f1a139aac8caa676b5cc74f9e06
Saurav Trivedi <ksaurav3045@gmail.com>
2024-11-10 19:43:10 +0530

    Removed eip to reduce cost

--- terraform-aws/output.tf
+++ terraform-aws/output.tf
@@ -3,10 +3,10 @@
 #   description = "Elastic IP for the database instance of "
 # }
 
-output "ui_api_instance_eip" {
-  value       = aws_eip.ui_api_instance_eip.public_ip
-  description = "Elastic IP for the UI/API instance of "
-}
+# output "ui_api_instance_eip" {
+#   value       = aws_eip.ui_api_instance_eip.public_ip
+#   description = "Elastic IP for the UI/API instance of "
+# }
 
 # output "acm_certificate_arn" {
 #   value = aws_acm_certificate.certificate.arn
