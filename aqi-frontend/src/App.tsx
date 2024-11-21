c2b959018664782ccf90e720f88ed1707772371d
saurav.kumar <saurav.kumar@colortokens.com>
2024-11-22 03:57:25 +0530

    Added Hotjar

--- aqi-frontend/src/App.tsx
+++ aqi-frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Hotjar from '@hotjar/browser';
 import AQIChart from './components/AQIChart';
 import OverallCard from './components/new-ui/OverallCard';
 import AQIDetails from './components/new-ui/AQIDetails';
 
-
-
 const App: React.FC = () => {
+    useEffect(() => {
+        const siteId = 5216984; // Replace with your actual Hotjar Site ID
+        const hotjarVersion = 6; // Use the appropriate Hotjar snippet version
+
+        Hotjar.init(siteId, hotjarVersion);
+    }, []); // Ensures this runs only once when the component mounts
+
     return (
         <Router>
             <Routes>
@@ -18,13 +25,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-
-// const App: React.FC = () => {
-
-//     return (
-//         <AQIChart />
-//     );
-// };
-
-// export default App;
