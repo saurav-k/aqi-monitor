eedf1fd452c942265fb1279ff311055ab357587c
saurav.kumar <saurav.kumar@colortokens.com>
2024-11-24 04:26:44 +0530

    Added weather_data_analysis for python

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,8 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sqlalchemy.orm import sessionmaker, declarative_base, SQLAlchemyError
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.exc import SQLAlchemyError
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
