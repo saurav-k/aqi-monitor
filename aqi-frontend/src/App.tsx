b96f4493f9f3f36dcf7953e7c87de0a479b10afd
saurav.kumar <saurav.kumar@colortokens.com>
2024-11-23 21:53:29 +0530

    Version 3.5 Removed hotjar

--- aqi-frontend/src/App.tsx
+++ aqi-frontend/src/App.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Hotjar from '@hotjar/browser';
+// import Hotjar from '@hotjar/browser';
 import AQIChart from './components/AQIChart';
 import OverallCard from './components/new-ui/OverallCard';
 import AQIDetails from './components/new-ui/AQIDetails';
 
 const App: React.FC = () => {
-    useEffect(() => {
-        const siteId = 5216984; // Replace with your actual Hotjar Site ID
-        const hotjarVersion = 6; // Use the appropriate Hotjar snippet version
+    // useEffect(() => {
+    //     const siteId = 5216984; // Replace with your actual Hotjar Site ID
+    //     const hotjarVersion = 6; // Use the appropriate Hotjar snippet version
 
-        Hotjar.init(siteId, hotjarVersion);
-    }, []); // Ensures this runs only once when the component mounts
+    //     Hotjar.init(siteId, hotjarVersion);
+    // }, []); // Ensures this runs only once when the component mounts
 
     return (
         <Router>
