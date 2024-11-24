c168e16a0a5a24859c846b53a415063575efa994
saurav.kumar <saurav.kumar@colortokens.com>
2024-11-24 07:05:10 +0530

    AQI Chart Refactor

--- aqi-frontend/src/App.tsx
+++ aqi-frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Hotjar from '@hotjar/browser';
-import AQIChart from './components/AQIChart';
+import AQIChart from './components/AQIChart/AQIChart';
 import OverallCard from './components/new-ui/OverallCard';
 import AQIDetails from './components/new-ui/AQIDetails';
 
--- aqi-frontend/src/types/aqiData.ts
+++ aqi-frontend/src/types/aqiData.ts
@@ -1,9 +1,9 @@
 export interface AQIData {
-    timestamp: string;
-    aqi_pm25: number;
-    aqi_pm10: number;
     pm25: number;
     pm10: number;
+    aqi_pm25: number;
+    aqi_pm10: number;
+    timestamp: string;
     overall_aqi: number;
 }
 
